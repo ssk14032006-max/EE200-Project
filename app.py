e252157c3b185538da1d2dc6f856f7659041a4b0
ssk14032006-max <ssk14032006@gmail.com>
2026-06-22 13:42:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ st.markdown(f"""
 # ----------------------------------------------------------------------
 @st.cache_resource
 def load_database():
-    with open("database.pkl", "rb") as f:
+    with open("database (2).pkl", "rb") as f:
         return pickle.load(f)
 
 
